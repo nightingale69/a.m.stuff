7919a005da39cb37e1f27e5072099b8121494a7a
Sergey Solovyev <draghignazzo.sss@gmail.com>
2018-11-30 17:16:31 +0300

    instagram in progress

--- src/app/home/home_social/social.component.ts
+++ src/app/home/home_social/social.component.ts
@@ -6,174 +6,39 @@ import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
   templateUrl: './social.component.html',
   styleUrls: ['./social.component.css']
 })
-export class SocialComponent implements OnInit {
+export class SocialComponent {
 
+  public carouselTileItems: Array<any> = 
+  [
+    {image: 'assets/images/instagram/slide1.jpg', text: 'ссылка 1', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide2.jpg', text: 'ссылка 2', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide3.jpg', text: 'ссылка 3', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide4.jpg', text: 'ссылка 4', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide5.jpg', text: 'ссылка 5', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide6.jpg', text: 'ссылка 6', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide1.jpg', text: 'ссылка 7', url:'https://www.instagram.com/p/BokXsYtFi-O/'},
 
-
-  /*imgags = [
-    'assets/images/bags/product4_1.jpg',
-    'assets/images/bags/product2_1.jpg',
-    'assets/images/bags/product12_3.jpg',
-    'assets/images/bags/product4_1.jpg'
+    {image: 'assets/images/instagram/slide4.jpg', text: 'ссылка 8', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide5.jpg', text: 'ссылка 9', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide6.jpg', text: 'ссылка 10', url:'https://www.instagram.com/p/BokXsYtFi-O/'}, 
+    {image: 'assets/images/instagram/slide1.jpg', text: 'ссылка 11', url:'https://www.instagram.com/p/BokXsYtFi-O/'}
   ];
-  public carouselTileItems: Array<any> = [0, 1, 2, 3, 4, 5, 6];
-  public carouselTiles = {
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: []
-  };
+
   public carouselTile: NguCarouselConfig = {
-    grid: { xs: 1, sm: 1, md: 4, lg: 6, all: 0 },
-    slide: 3,
-    speed: 250,
+    grid: { xs: 2, sm: 3, md: 4, lg: 6, all: 0 },
+    slide: 1,
+    speed: 450,
+    interval: {
+      timing: 6000,
+      initialDelay: 1000
+    },
     point: {
       visible: true
     },
     load: 2,
     velocity: 0,
+    loop: true,
     touch: true,
     easing: 'cubic-bezier(0, 0, 0.2, 1)'
   };
-  constructor() {}
- 
-  ngOnInit() {
-    this.carouselTileItems.forEach(el => {
-      this.carouselTileLoad(el);
-    });
-  }
- 
-  public carouselTileLoad(j) {
-    // console.log(this.carouselTiles[j]);
-    const len = this.carouselTiles[j].length;
-    if (len <= 30) {
-      for (let i = len; i < len + 15; i++) {
-        this.carouselTiles[j].push(
-          this.imgags[Math.floor(Math.random() * this.imgags.length)]
-        );
-      }
-    }
-  }*/
-
-
-  
-
-
-  twists = 
-  	[
-  	  {
-		url: "5knifes",
-  	    image1: "assets/images/bags/product12_1.jpg",
-  	    image2: "assets/images/bags/product12_2.jpg",
-  	    image3: "assets/images/bags/product12_3.jpg",
-  	    description: 'Для 5 поварских ножей / инструментов', 
-  	    price: '6000 ₽'
-  	  },
-  	  {
-		url: "6knifes",
-  	    image1: "assets/images/bags/product4_1.jpg",
-  	    image2: "assets/images/bags/product4_2.jpg",
-  	    image3: "assets/images/bags/product4_3.jpg",
-  	    description: 'Для 6 поварских ножей / инструментов', 
-  	    price: '6500 ₽'
-  	  },
-  	  {
-		url: "7knifes",
-  	    image1: "assets/images/bags/product11_1.jpg",
-  	    image2: "assets/images/bags/product11_2.jpg",
-  	    image3: "assets/images/bags/product11_3.jpg",
-  	    description: 'Для 7 поварских ножей / инструментов', 
-  	    price: '5000 ₽'
-  	  },
-  	  {
-		url: "10knifes",
-  	    image1: "assets/images/bags/product9_1.jpg",
-  	    image2: "assets/images/bags/product9_2.jpg",
-  	    image3: "assets/images/bags/product9_3.jpg",
-  	    description: 'Для 10 поварских ножей / инструментов', 
-  	    price: '6900 ₽'
-  	  },
-  	  {
-		url: "13knifes",
-  	    image1:  "assets/images/bags/product1_1.jpg",
-  	    image2:  "assets/images/bags/product1_2.jpg",
-  	    image3:  "assets/images/bags/product1_3.jpg",
-  	    description: 'Для 13 поварских ножей / инструментов', 
-  	    price: '8900 ₽'
-  	  },
-  	  {
-		url: "18knifes",
-  	    image1: "assets/images/bags/product10_1.jpg",
-  	    image2: "assets/images/bags/product10_2.jpg",
-  	    image3: "assets/images/bags/product10_3.jpg",
-  	    description: 'Для 18 поварских ножей / инструментов', 
-  	    price: '15000 ₽'
-  	  },
-  	  /*{
-  	    image1: "assets/images/bags/product2_1.jpg",
-  	    image2: "assets/images/bags/product2_2.jpg",
-  	    image3: "assets/images/bags/product2_3.jpg",
-  	    description: 'Скрутка для 7 поварских инструментов, скрутка для ножей', 
-  	    price: '6700 руб.'
-  	  },
-  	  {
-  	    image1: "assets/images/bags/product3_1.jpg",
-  	    image2: "assets/images/bags/product3_2.jpg",
-  	    image3: "assets/images/bags/product3_3.jpg",
-  	    description: 'Скрутка для 13 поварских инструментов, скрутка для ножей', 
-  	    price: '8900 руб.'
-  	  },
-  	  {
-  	    image1: "assets/images/bags/product5_1.jpg",
-  	    image2: "assets/images/bags/product5_2.jpg",
-  	    image3: "assets/images/bags/product5_3.jpg",
-  	    description: 'Скрутка для 18 поварских инструментов, скрутка для ножей', 
-  	    price: '15000 руб.'
-  	  },
-  	  {
-  	    image1: "assets/images/bags/product6_1.jpg",
-  	    image2: "assets/images/bags/product6_2.jpg",
-  	    image3: "assets/images/bags/product6_3.jpg",
-  	    description: 'Скрутка для 10 поварских инструментов скрутка для ножей', 
-  	    price: '6900 руб.'
-  	  },
-  	  {
-  	    image1: "assets/images/bags/product7_1.jpg",
-  	    image2: "assets/images/bags/product7_2.jpg",
-  	    image3: "assets/images/bags/product7_3.jpg",
-  	    description: 'Скрутка для 10 поварских инструментов скрутка для ножей', 
-  	    price: '6900 руб.'
-  	  },*/
-	];
-
-
-
-
-
-  ngOnInit() {
-    this.carouselBanner = {
-      grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
-      slide: 1,
-      speed: 400,
-      interval: 4000,
-      point: {
-        visible: true
-      },
-      load: 2,
-      loop: true,
-      touch: true, // touch is not currently in active for vertical carousel, will enable it in future build
-      vertical:  {
-        enabled: true,
-        height: 400
-      }
-    };
-  }
-
-
-
-
-
 }
