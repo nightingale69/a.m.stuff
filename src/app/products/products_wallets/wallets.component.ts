8f9237bc327e85b0e516c801b1b3f417985658a1
draghignazzo.sss@gmail.com <draghignazzo.sss@gmail.com>
2018-12-14 22:02:03 +0300

    commit

--- src/app/products/products_wallets/wallets.component.ts
+++ src/app/products/products_wallets/wallets.component.ts
@@ -5,11 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './wallets.component.html',
   styleUrls: ['./wallets.component.css']
 })
-export class WalletsComponent implements OnInit {
+export class WalletsComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    wallets = 
+  	[
+  	  {
+		url: "blue_wallet",
+  	    image: "assets/images/products/wallets/wallet_blue.jpg",
+  	    description: 'Кошелёк синий', 
+  	    price: '1000 ₽'
+  	  },
+  	  {
+		url: "red_wallet",
+  	    image: "assets/images/products/wallets/wallet_red.jpg",
+  	    description: 'Кошелёк красный', 
+  	    price: '1000 ₽'
+  	  },
+  	  {
+		url: "orange_wallet",
+  	    image: "assets/images/products/wallets/wallet_orange.jpg",
+  	    description: 'Кошелёк оранжевый', 
+  	    price: '1000 ₽'
+  	  },
+  	  {
+		url: "dark_brown_wallet",
+  	    image: "assets/images/products/wallets/wallet_dark_brown.jpg",
+  	    description: 'Кошелёк тёмно-коричневый', 
+  	    price: '1000 ₽'
+  	  },
+  	  {
+		url: "brown_wallet",
+  	    image:  "assets/images/products/wallets/wallet_brown.jpg",
+  	    description: 'Кошелёк коричневый', 
+  	    price: '1000 ₽'
+  	  }
+	];
 }
